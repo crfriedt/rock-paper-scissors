cd32ba98ea726ae8b3a9021bc5f716ad4b0328b5
Cory Friedt <c.friedt89@gmail.com>
2020-04-27 20:04:15 -0700

    implemented alert functionality

--- app.js
+++ app.js
@@ -3,52 +3,114 @@ const btnOne = document.getElementById('btn1'),
       btnTwo = document.getElementById('btn2'),
       btnThree = document.getElementById('btn3'),
       computerTxt = document.getElementById('computerText'),
-      computerChoiceImg = document.getElementById('computerChoiceImg');
+      computerChoiceImg = document.getElementById('computerChoiceImg'),
+      alert = document.getElementById('alert');
+
 
 
 // Event Handlers
 btnOne.addEventListener('click', function(){
   randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log(randomNumber);
   if (randomNumber === 1){
+    alert.className = 'alert-dark';
+    alert.innerHTML = 'It is a tie !'
     computerChoiceImg.src = 'images/rock.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   } else if (randomNumber === 2){
+    alert.className = 'alert-danger';
+    alert.innerHTML = 'You lost !'
     computerChoiceImg.src = 'images/paper.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   } else if (randomNumber === 3){
+    alert.className = 'alert-success';
+    alert.innerHTML = 'You won !'
     computerChoiceImg.src = 'images/scissors.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   }
 });
 
 btnTwo.addEventListener('click', function(){
   randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log(randomNumber);
   if (randomNumber === 1){
+    alert.className = 'alert-danger';
+    alert.innerHTML = 'You lost !'
     computerChoiceImg.src = 'images/rock.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   } else if (randomNumber === 2){
+    alert.className = 'alert-dark';
+    alert.innerHTML = 'It is a tie !'
     computerChoiceImg.src = 'images/paper.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   } else if (randomNumber === 3){
+    alert.className = 'alert-danger';
+    alert.innerHTML = 'You lost !'
     computerChoiceImg.src = 'images/scissors.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   }
 });
 
 btnThree.addEventListener('click', function(){
   randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log(randomNumber);
   if (randomNumber === 1){
+    alert.className = 'alert-danger';
+    alert.innerHTML = 'You lost !'
     computerChoiceImg.src = 'images/rock.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   } else if (randomNumber === 2){
+    alert.className = 'alert-success';
+    alert.innerHTML = 'You won !'
     computerChoiceImg.src = 'images/paper.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   } else if (randomNumber === 3){
+    alert.className = 'alert-dark';
+    alert.innerHTML = 'It is a tie !'
     computerChoiceImg.src = 'images/scissors.png';
     computerChoiceImg.style = "height: 300px";
+    setTimeout(function(){
+      computerChoiceImg.src = '';
+      alert.className = '';
+      alert.innerHTML = '';
+    }, 2000);
   }
 });
 
